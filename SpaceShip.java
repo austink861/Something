294dd5b0f81d9b67ecb81a10e92a11b8de58485e
austink861 <112571817+austink861@users.noreply.github.com>
2022-10-03 16:51:34 -0500

    Update SpaceShip.java

--- SpaceShip.java
+++ SpaceShip.java
@@ -4,6 +4,6 @@ public abstract class SpaceShip {
 	protected int numEngines; //Variables are protected so that only child classes can access them.
 	protected String name;
 	
-	void ShowSpeed() {};  //This creates the method to be overwritten by the other classes.
+	abstract void ShowSpeed() {};  //This creates the method to be overwritten by the other classes.
 
 }
